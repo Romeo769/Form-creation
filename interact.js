8c3dab611a964f760c6b557c92a9cc2cd9ef50df
Romeo769 <romeoludovickouane@gmail.com>
2026-07-23 20:54:04 +0000

    version 3.0

---
+++ interact.js
@@ -0,0 +1,17 @@
+var name = "Romeo";
+console.log(name);
+
+let age = 55;
+age = 60;
+console.log(age);
+
+const color = 'orange';
+console.log(color);
+let login = false;
+console.log(login);
+let person={
+    names : "James",
+    height : 20,
+    isFair : true
+}
+console.log(person);
